bd174473e5d6d9ca5b18d6d968ecf810ad505750
SomeshChevella <chevellavsomeshwar@gmail.com>
2024-09-07 15:22:46 -0400

    Add ProductServiceTest

---
+++ src/test/java/com/grocery/rest/webservices/grocery_app/product/ProductServiceTest.java
@@ -0,0 +1,23 @@
+package com.grocery.rest.webservices.grocery_app.product;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ProductServiceTest {
+
+    @Test
+    void getAllProducts() {
+    }
+
+    @Test
+    void getProductById() {
+    }
+
+    @Test
+    void createProduct() {
+    }
+
+    @Test
+    void deleteProductById() {
+    }
+}
