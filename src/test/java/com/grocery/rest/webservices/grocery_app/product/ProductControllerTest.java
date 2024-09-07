4c7ca7979257fb6c563b924b01990bf57b8a75c2
SomeshChevella <chevellavsomeshwar@gmail.com>
2024-09-07 14:00:41 -0400

    add a small annotation on ProductControllerTest

--- src/test/java/com/grocery/rest/webservices/grocery_app/product/ProductControllerTest.java
+++ src/test/java/com/grocery/rest/webservices/grocery_app/product/ProductControllerTest.java
@@ -13,6 +13,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -21,6 +22,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 
+@SpringBootTest
 class ProductControllerTest {
 	
 	@Autowired
